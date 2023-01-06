9d5cfd60585a37f43c76f681abccc79b867d61ed
koha11 <khoa88108@gmail.com>
2023-01-07 01:28:38 +0700

    update text.js

--- assets/JS/text.js
+++ assets/JS/text.js
@@ -1,36 +1,47 @@
 export const textList = [
   {
     span: 'My sao, đúng thật là cậu rồi, quả là 1 cái tên siu cute nhỉ !',
+    id: 1 
   },
   {
     span: 'Hmmm, Tớ có 1 điều quan trọng muốn nói với cậu...',
+    id: 2
   },
   {
     span: 'Tớ biết, là cậu luôn muốn có 1 lời tỏ tình lãng mạn giống như những người khác.',
+    id: 3
   },
   {
     span: 'Thì đây sẽ là chiếc web mà tớ code vụng để thể hiện tình cảm của mình dành cho cậu..',
+    id: 4
   },
   {
     span: 'Cậu đối với tớ như mặt trời vậy, luôn dẫn đường tớ hướng về phía trước.',
+    id: 5
   },
   {
     span: 'Cậu tỏa ra hơi ấm, tỏa ra tiếng cười, khiến 2 mắt tớ không tài nào có thể mở to ra được, 2 má thì không ngừng hóp lại vì đỏ mặt.',
+    id: 6
   },
   {
     span: 'Nhưng chả hiểu từ lúc nào, chẳng kịp nhận ra nữa, tớ đã yêu cậu nhiều lắm rồi, muốn được chở che, chăm sóc cho cậu.',
+    id: 7
   },
   {
     span: 'Tớ mê cậu ở mọi thứ, đặc biệt là sự cute của cậu, khiến mình mãi chìm đắm trong tình yêu.',
+    id: 8
   },
   {
     span: 'Nên là...',
+    id: 9
   },
   {
     span: 'Cậu hãy cho phép tớ vẽ nên 1 tương lai xa, nơi mà có cả 2 ta đứng cùng, nhé!',
+    id: 10
   },
   {
     span: 'Tớ yêu cậu.',
+    id: 11
   },
 
 ]
